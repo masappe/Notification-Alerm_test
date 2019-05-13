4cdafde1bb0b6ee3fe00dfe1e5a8cbc7ece7bc04
masappe <masato6820@gmail.com>
2019-05-13 09:52:29 +0900

    Initial Commit

---
+++ Notification,Alerm_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notification,Alerm_test
+//
+//  Created by Masato Hayakawa on 2019/05/13.
+//  Copyright © 2019 masappe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
